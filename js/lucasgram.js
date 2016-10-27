1b9daefb05695bac38399315aeff9b432caa9d26
lucassandes <lucassandes@msn.com>
2016-10-27 17:53:40 -0200

    Add custom filter upload

--- js/lucasgram.js
+++ js/lucasgram.js
@@ -29,23 +29,36 @@ $("#close-modal").click(function () {
 
 //uploader class
 var uploader = new Dropzone('#my-dropzone');
-
+var isCustomFilter = false;
+var appliedFilter = null;
+function applyFilter (image, isCustomFilter) {
+    if (isCustomFilter) {
+        console.log("true");
+        image.css("-webkit-filter", customFilterCSS);
+        image.css("-moz-filter", customFilterCSS);
+        image.css("-ms-filter", customFilterCSS);
+        image.css("-o-filter", customFilterCSS);
+        image.css("filter", customFilterCSS);
+    } else {
+        image.addClass(appliedFilter)
+    }
+}
 uploader.on('success', function (file, resp) {
     $("#next-button").removeClass("disabled").attr('title', 'Next to chose filters').attr('data-original-title', 'Next to chose filters');
     console.log('beleza');
     console.log(file.name);
 
-
+    var uploadedImage = 'uploads/' + file.name;
     $("#next-button").click(function () {
-        $("#uploaded-image").attr('src', 'uploads/' + file.name);
-        $(".uploaded-image-thumb").attr('src', 'uploads/' + file.name);
+        $("#uploaded-image").attr('src', uploadedImage);
+        $(".uploaded-image-thumb").attr('src', uploadedImage);
 
         $("#my-dropzone").hide();
         $("#apply-filters").show();
         $("#myModalLabel").html('Chose a filter or <a href="#" id="create-filter-btn">create your own!</a>');
 
         //apply filters
-        var appliedFilter;
+
         //no-filter
         $("#filter-1").click(function () {
             $("#uploaded-image").removeClass().addClass('img-responsive');
@@ -83,23 +96,30 @@ uploader.on('success', function (file, resp) {
         });
 
 
+
         // create filters
         $("#create-filter-btn").click(function () {
             $("#create-filter").show();
             $("#apply-filters").hide();
-            $("#create-filter-img").attr('src', 'uploads/' + file.name);
-
+            $("#create-filter-img").attr('src', uploadedImage);
+            isCustomFilter = true;
 
         });
 
+        //share-to
         $("#next-button").click(function () {
-            //share-to
+
+
+            var pictureToShare = $("#picture-to-share").attr('src', uploadedImage);
+            console.log("aqui");
+            console.log(customFilterCSS);
+            applyFilter(pictureToShare, isCustomFilter);
+
+
 
             $("#myModalLabel").html('Share');
-
-
-            $("#picture-to-share").attr('src', 'uploads/' + file.name).addClass(appliedFilter);
             $("#apply-filters").hide();
+            $("#create-filter").hide();
             $("#share-to").show();
 
 
@@ -107,10 +127,11 @@ uploader.on('success', function (file, resp) {
                 $("#next-button").html('Share').attr('data-dismiss', 'modal');
                 var location = $("#input-location").val();
                 var caption = $("#input-caption").val();
-
+                var uploadedImage = $("#upload-image").attr('src', 'uploads/' + file.name);
                 $("#upload-location").html(location);
                 $("#upload-caption").html(caption);
-                $("#upload-image").attr('src', 'uploads/' + file.name).addClass(appliedFilter);
+
+                applyFilter(uploadedImage, isCustomFilter);
 
                 $("#upload-article").fadeIn("slow", function () {
                     // Animation complete
@@ -131,7 +152,7 @@ function rewriteSliderValue(n) {
 
 }
 
-
+var customFilterCSS = '';
 $(document).ready(function() {
     //thanks http://css3.bradshawenterprises.com/filters/
     var controls_input = $("#controls input"),
@@ -155,7 +176,8 @@ $(document).ready(function() {
 
 
             newCSS += $(this).attr('id')+"("+value+units+") ";
-
+            customFilterCSS = newCSS;
+            console.log(customFilterCSS);
             $("#"+$(this).attr('id')+"_output").text($(this).attr('id')+"("+value+units+")");
         })
         image.css("-webkit-filter", newCSS);
