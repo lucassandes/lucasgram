3c6791108f0a833e00a392db2cb8b211ad7d94e1
lucassandes <lucassandes@msn.com>
2016-10-27 23:18:07 -0200

    Fix tabs navigation bug

--- js/lucasgram.js
+++ js/lucasgram.js
@@ -4,16 +4,15 @@
 //starting bootstrap tooltip
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
-})
+});
 
 function startModal() {
     $('#my-dropzone').show();
-    $("#apply-filters").hide();
+    $("#filter-or-edit").hide();
     $("#share-to").hide();
     $("#upload-article").hide();
-    $("#create-filter").hide();
 }
-;
+
 //disable button until user uploads img
 $(document).ready(function () {
     startModal();
@@ -31,18 +30,22 @@ $("#close-modal").click(function () {
 var uploader = new Dropzone('#my-dropzone');
 var isCustomFilter = false;
 var appliedFilter = null;
-function applyFilter (image, isCustomFilter) {
+function applyFilter(image, isCustomFilter) {
+
     if (isCustomFilter) {
-        console.log("true");
         image.css("-webkit-filter", customFilterCSS);
         image.css("-moz-filter", customFilterCSS);
         image.css("-ms-filter", customFilterCSS);
         image.css("-o-filter", customFilterCSS);
         image.css("filter", customFilterCSS);
-    } else {
-        image.addClass(appliedFilter)
     }
+    else {
+        image.addClass(appliedFilter);
+    }
+
 }
+
+
 uploader.on('success', function (file, resp) {
     $("#next-button").removeClass("disabled").attr('title', 'Next to chose filters').attr('data-original-title', 'Next to chose filters');
     console.log('beleza');
@@ -54,7 +57,7 @@ uploader.on('success', function (file, resp) {
         $(".uploaded-image-thumb").attr('src', uploadedImage);
 
         $("#my-dropzone").hide();
-        $("#apply-filters").show();
+        $("#filter-or-edit").show();
         $("#myModalLabel").html('Chose a filter or <a href="#" id="create-filter-btn">create your own!</a>');
 
         //apply filters
@@ -95,31 +98,18 @@ uploader.on('success', function (file, resp) {
             $("#uploaded-image").removeClass().addClass('img-responsive brooklyn');
         });
 
-
-
         // create filters
-        $("#create-filter-btn").click(function () {
-            $("#create-filter").show();
-            $("#apply-filters").hide();
-            $("#create-filter-img").attr('src', uploadedImage);
-            isCustomFilter = true;
+        $(".uploaded-image").attr('src', uploadedImage);
 
-        });
 
         //share-to
         $("#next-button").click(function () {
 
-
+            ($("#has-custom-filter").hasClass('active')) ? isCustomFilter = true : isCustomFilter = false;
             var pictureToShare = $("#picture-to-share").attr('src', uploadedImage);
-            console.log("aqui");
-            console.log(customFilterCSS);
             applyFilter(pictureToShare, isCustomFilter);
-
-
-
             $("#myModalLabel").html('Share');
-            $("#apply-filters").hide();
-            $("#create-filter").hide();
+            $("#filter-or-edit").hide();
             $("#share-to").show();
 
 
@@ -146,22 +136,21 @@ uploader.on('success', function (file, resp) {
 });
 
 
-
 function rewriteSliderValue(n) {
-    return 1 + (n/100)
+    return 1 + (n / 100)
 
 }
 
 var customFilterCSS = '';
-$(document).ready(function() {
+$(document).ready(function () {
     //thanks http://css3.bradshawenterprises.com/filters/
     var controls_input = $("#controls input"),
-        image = $("#create-filter-img");
+        image = $(".uploaded-image");
 
     function updateDisplay() {
         var newCSS = '';
         var value;
-        controls_input.each(function(){
+        controls_input.each(function () {
             var units = '';
             value = rewriteSliderValue($(this).val());
             if ($(this).attr('id') == 'blur') {
@@ -170,15 +159,15 @@ $(document).ready(function() {
             } else if ($(this).attr('id') == 'hue-rotate') {
                 units = 'deg';
                 value = $(this).val();
-            } else if ($(this).attr('id') == 'sepia' ) {
+            } else if ($(this).attr('id') == 'sepia') {
                 value = $(this).val();
             }
 
 
-            newCSS += $(this).attr('id')+"("+value+units+") ";
+            newCSS += $(this).attr('id') + "(" + value + units + ") ";
             customFilterCSS = newCSS;
             console.log(customFilterCSS);
-            $("#"+$(this).attr('id')+"_output").text($(this).attr('id')+"("+value+units+")");
+            $("#" + $(this).attr('id') + "_output").text($(this).attr('id') + "(" + value + units + ")");
         })
         image.css("-webkit-filter", newCSS);
         image.css("-moz-filter", newCSS);
@@ -186,12 +175,13 @@ $(document).ready(function() {
         image.css("-o-filter", newCSS);
         image.css("filter", newCSS);
     }
+
     updateDisplay();
 
     controls_input.change(updateDisplay);
 
-    $("#reset").click(function(){
-        controls_input.each(function(){
+    $("#reset").click(function () {
+        controls_input.each(function () {
             $(this).val($(this).attr('data-default'));
         });
         updateDisplay();
